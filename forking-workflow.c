10b895ff59dde7de92cca1d419e6dcef96d0d8e7
Jekoi1 <124031113+Jekoi1@users.noreply.github.com>
2023-03-07 20:29:23 +0800

    Update forking-workflow.c

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,11 +9,13 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
+	Lebumfacil_printName();
 	
 	return 0;
 }
 
 
 // Refer to the function name for the naming of your funcion.
-void navarro_printName() {
+void Lebumfacil_printName() {
+	printf("-NJ V. Lebumfacil-");
 }

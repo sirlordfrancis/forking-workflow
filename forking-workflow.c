ec0b2852d2b33385b88006d5cd56114dd3263b2b
TJ Ramirez <ramirez.theodan_john@hnu.edu.ph>
2023-03-07 15:45:16 +0800

    added name function

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,11 +9,15 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
-	
+	ramirez_printName();
 	return 0;
 }
 
 
 // Refer to the function name for the naming of your funcion.
 void navarro_printName() {
+}
+
+void ramirez_printName() {
+	printf("Theodan John Ramirez");
 }

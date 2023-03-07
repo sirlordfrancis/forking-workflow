cdfdf89d9946526750e44f569d26f1ea52397cd2
Dubst <dubstepbiatch@gmail.com>
2023-03-07 11:43:48 +0800

    Added Print Function

--- forking-workflow.c
+++ forking-workflow.c
@@ -9,11 +9,15 @@ int main(){
 	printf("Your name must be printed if this program is run. \n\n");
 	
 	// Call your function below this line.
-	
+	baliwis_printName();	
+
 	return 0;
 }
 
 
 // Refer to the function name for the naming of your funcion.
 void navarro_printName() {
+}
+void baliwis_printName() {
+	printf("Joreme N. Baliwis");
 }
